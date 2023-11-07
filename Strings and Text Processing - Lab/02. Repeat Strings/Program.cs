4431ee5c414d819d3ccd6a49026c75eeaba4de09
Filip-Peev <Filip.Peev.Official@gmail.com>
2023-11-07 04:50:06 +0200

    updated

---
+++ Strings and Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] text = Console.ReadLine().Split();
+
+string output = "";
+
+for (int i = 0; i < text.Length; i++)
+{
+    string currentText = text[i];
+    for (int j = 0; j < currentText.Length; j++)
+    {
+        output += currentText;
+    }
+}
+Console.WriteLine(output);
---
+++ Strings and Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,11 @@
+﻿string wordToRemove = Console.ReadLine();
+
+string text = Console.ReadLine();
+
+while (text.Contains(wordToRemove)){
+
+    int indexOfTextToRemove = text.IndexOf(wordToRemove);
+    text = text.Remove(indexOfTextToRemove, wordToRemove.Length);     
+}
+
+Console.WriteLine(text);
