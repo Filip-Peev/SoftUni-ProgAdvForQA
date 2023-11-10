8b01b62f64df1299c795f5b448f9cb0984563c3a
Filip-Peev <Filip.Peev.Official@gmail.com>
2023-11-10 22:38:16 +0200

    regex

---
+++ Strings and Text Processing - Lab/Test2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using System.Text;
+for (int i = 0; i <= 5; i++)
+{
+        Stopwatch sw = new Stopwatch();
+    sw.Start();
+    StringBuilder text = new StringBuilder();
+    for (int j = 0; j < 100000000; j++)
+        text.Append(j);
+    sw.Stop();
+    Console.WriteLine(sw.ElapsedMilliseconds);
+
+}
