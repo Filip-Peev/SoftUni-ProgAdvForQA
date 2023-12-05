5de1e25eced332f50c98a7bc09025646b4e5f1df
Filip-Peev <Filip.Peev.Official@gmail.com>
2023-12-05 05:04:22 +0200

    added second exercise

---
+++ 2. Dictionaries, Lambda and LINQ - Exercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,31 @@
+﻿Dictionary<char, int> characters = new();
+
+string input = Console.ReadLine();
+
+foreach (char ch in input)
+{
+    if (ch == ' ')
+    {
+        continue;
+    }
+    //if (characters.ContainsKey(ch))
+    //{
+    //    characters[ch]+=1;
+    //}
+    //else
+    //{
+    //    characters.Add(ch, 1);
+    //}
+    if (!characters.ContainsKey(ch))
+    {
+        characters.Add(ch, 1);
+    }
+    else
+    {
+        characters[ch] += 1;
+    }
+}
+foreach (KeyValuePair <char, int> pair in characters)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
---
+++ 2. Dictionaries, Lambda and LINQ - Exercise/05. Student Academy/Program.cs
@@ -0,0 +1,23 @@
+﻿Dictionary<string, List<double>> students = new();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string student = Console.ReadLine();
+    double grade = double.Parse(Console.ReadLine());
+
+    if (!students.ContainsKey(student))
+    {
+        students.Add(student, new List<double>());
+    }
+
+    students[student].Add(grade);
+}
+
+var excelent = students.Where(st => st.Value.Average() >= 4.50);
+
+foreach (var currentStudent in excelent)
+{
+    Console.WriteLine($"{currentStudent.Key} -> {currentStudent.Value.Average():F2}");
+}
