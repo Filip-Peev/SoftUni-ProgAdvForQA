9f3086c1ed83b2658eaaac8e2f56615fd095d322
Filip-Peev <Filip.Peev.Official@gmail.com>
2023-12-16 17:50:33 +0200

    .

---
+++ 4. Objects and Classes - Exercise - Unfinished/01. Students/Student.cs
@@ -0,0 +1,32 @@
+﻿internal class Student
+{
+    // fields -> полета, характеристики на класа
+    private string firstName;
+    private string lastName;
+    private double grade;
+
+    // properties -> дава ни достъп до полетата
+    public string FirstName
+    {
+        get { return firstName; }
+        set { firstName = value; }
+    }
+    public string LastName
+    {
+        get { return lastName; }
+        set { lastName = value; }
+    }
+    public double Grade
+    {
+        get { return grade; }
+        set { grade = value; }
+    }
+
+    // constructor -> метод, чрез който създаваме обекти от класа
+    public Student (string firstName, string lastName, double grade)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Grade = grade;
+    }
+}
---
+++ 4. Objects and Classes - Exercise - Unfinished/02. Articles/Article.cs
@@ -0,0 +1,34 @@
+﻿class Article
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+
+    // constructor
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    // Действия - методи:
+    public void Edit(string newContent)
+    {
+        Content = newContent;
+    }
+    public void ChangeAuthor(string newAuthor)
+    {
+        Author = newAuthor;
+    }
+    public void Rename(string newTitle)
+    {
+        Title = newTitle;
+    }
+
+    // пренаписваме метода .ToString()
+    public override string ToString()
+    {
+        return $"{Title} - {Content}: {Author}";
+    }
+}
